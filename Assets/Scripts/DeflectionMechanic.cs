b13099940a003a9951adf17cfb7c2b6b0173ffbb
Alex Deane <alexrdeane@hotmail.com>
2019-08-08 18:42:59 +1000

    deflection jenk proto

---
+++ Assets/Scripts/DeflectionMechanic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+public class DeflectionMechanic : MonoBehaviour
+{
+    public Collider col;
+    public GameObject bullet;
+    public GameObject clone;
+    public bool bulletIn;
+    public Transform spawnPoint;
+
+    private void Start()
+    {
+        clone = bullet;
+    }
+    private void Update()
+    {
+        if (bulletIn == true)
+        {
+            Instantiate(clone, spawnPoint.position, spawnPoint.rotation);
+            bulletIn = false;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public GameObject clone;
+    public Transform spawnPoint;
+    void Start()
+    {
+        clone = bullet;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(clone, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
